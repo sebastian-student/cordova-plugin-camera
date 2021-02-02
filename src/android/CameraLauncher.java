46e964c0fa09bef7520fe61668e36b7e467a0324
Pieter Van Poyer <pieter.vanpoyer@portofantwerp.com>
2021-02-02 19:43:26 +0100

    Bugfix issue 665 (#700)
    * GH-665 - store the imageFilePath when the app is paused (onSaveInstance) and restore it back.
    
    * Update src/android/CameraLauncher.java whitespace layout
    
    Co-authored-by: Tim Brust <github@timbrust.de>
    
    Co-authored-by: Tim Brust <github@timbrust.de>
    (cherry picked from commit f70468920041134730b207e3ac9ca2597d6dbbe4)

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -94,6 +94,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
     private static final String GET_All = "Get All";
     private static final String CROPPED_URI_KEY = "croppedUri";
     private static final String IMAGE_URI_KEY = "imageUri";
+    private static final String IMAGE_FILE_PATH_KEY = "imageFilePath";
 
     private static final String TAKE_PICTURE_ACTION = "takePicture";
     private static final String HUAWEI_MANUFACTURER = "Huawei";
@@ -1428,6 +1429,10 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
             state.putString(IMAGE_URI_KEY, this.imageUri.getFileUri().toString());
         }
 
+        if (this.imageFilePath != null) {
+            state.putString(IMAGE_FILE_PATH_KEY, this.imageFilePath);
+        }
+
         return state;
     }
 
@@ -1451,6 +1456,10 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
         if (state.containsKey(IMAGE_URI_KEY)) {
             //I have no idea what type of URI is being passed in
             this.imageUri = new CordovaUri(Uri.parse(state.getString(IMAGE_URI_KEY)), cordova.getActivity());
+        }
+
+        if (state.containsKey(IMAGE_FILE_PATH_KEY)) {
+           this.imageFilePath = state.getString(IMAGE_FILE_PATH_KEY);
         }
 
         this.callbackContext = callbackContext;
