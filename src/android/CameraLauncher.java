0e9fee938ad108f4642eddd9fe6c2d9e10fa40de
David Boho <david.boho@tu-ilmenau.de>
2021-03-24 09:11:57 +0100

    may fix RecoverableSecurityException

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -974,10 +974,10 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
         try {
             Uri imageCollection;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                imageCollection = MediaStore.Audio.Media
+                imageCollection = MediaStore.Images.Media
                         .getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
             } else {
-                imageCollection = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                imageCollection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
             }
             uri = this.cordova.getActivity().getContentResolver().insert(imageCollection, values);
         } catch (RuntimeException e) {
@@ -1318,9 +1318,9 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
     private Uri whichContentStore() {
         if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                return MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+                return MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
             } else {
-                return MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                return MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
             }
         } else {
             return android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI;
